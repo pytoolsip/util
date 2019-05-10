dab15d58bed1b570a82b6b7caf9cf79af37b8588
JinZhang <2636423765@qq.com>
2019-05-10 14:46:49 +0800

    refactor:修改编译py文件脚本

--- compile/build.py
+++ compile/build.py
@@ -2,8 +2,8 @@
 # @Author: JinZhang
 # @Date:   2019-05-10 12:29:30
 # @Last Modified by:   JinZhang
-# @Last Modified time: 2019-05-10 13:28:46
-import os,json;
+# @Last Modified time: 2019-05-10 14:26:29
+import os,json,shutil;
 import compileall;
 
 def getJsonConfig():
@@ -11,6 +11,11 @@ def getJsonConfig():
     with open("config.json", "rb") as f:
         config = json.loads(f.read().decode("utf-8", "ignore"));
     return config;
+
+def copyDir(srcDir, targetDir):
+    tdPath = os.path.join(targetDir, os.path.basename(srcDir));
+    shutil.copytree(srcDir, tdPath);
+    return tdPath;
 
 def compileDir(dirPath, isRemoveOri):
 	compileall.compile_dir(dirPath);
@@ -25,5 +30,8 @@ if __name__ == '__main__':
     cfg = getJsonConfig();
     # 转换路径
     ppd = os.path.abspath(cfg["py_project_dir"]);
+    pptd = os.path.abspath(cfg["py_project_target_dir"]);
+    # 拷贝工程文件
+    tdPath = copyDir(ppd, pptd);
     # 编译文件
-    compileDir(ppd, cfg.get("is_remove_original", True));
+    compileDir(tdPath, cfg.get("is_remove_original", True));
