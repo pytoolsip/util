ad992ced553e3039b777d9d7340b80e7cc7672ff
JimDreamHeart <15602291936@163.com>
2019-05-11 18:10:46 +0800

    refactor:修改编码文件过程中的拷贝工程逻辑

--- compile/build.py
+++ compile/build.py
@@ -3,7 +3,7 @@
 # @Date:   2019-05-10 12:29:30
 # @Last Modified by:   JinZhang
 # @Last Modified time: 2019-05-10 14:26:29
-import os,json,shutil;
+import os,json,shutil,re;
 import compileall;
 
 def getJsonConfig():
@@ -12,15 +12,53 @@ def getJsonConfig():
         config = json.loads(f.read().decode("utf-8", "ignore"));
     return config;
 
+# 拷贝工程
+def copyProject(sPath, tPath):
+	if not os.path.exists(tPath):
+		os.makedirs(tPath);
+	for name in os.listdir(sPath):
+		# 过滤git文件
+		if re.search("^\.git.*", os.path.basename(name)):
+			continue;
+		# 拼接地址
+		srcPath = os.path.join(sPath, name);
+		targetPath = os.path.join(tPath, name);
+		if os.path.basename(name) in ["assets", "common"]:
+			copyProject(srcPath, targetPath);
+			continue;
+		if os.path.isfile(srcPath):
+			shutil.copyfile(srcPath, targetPath);
+		else:
+			shutil.copytree(srcPath, targetPath);
+
 def copyDir(srcDir, targetDir):
-    tdPath = os.path.join(targetDir, os.path.basename(srcDir));
-    shutil.copytree(srcDir, tdPath);
-    return tdPath;
+	tdPath = os.path.join(targetDir, "PyToolsIP");
+	copyProject(srcDir, tdPath);
+	return tdPath;
 
 def compileDir(dirPath, isRemoveOri):
 	compileall.compile_dir(dirPath);
 	if isRemoveOri:
+		# 移动dirPath下__pycache__里的文件，并移除py文件
 		for root, dirs, files in os.walk(dirPath):
+			for path in dirs:
+				if os.path.basename(path) != "__pycache__":
+					continue;
+				pycachePath = os.path.join(root,path);
+				for name in os.listdir(pycachePath):
+					fPath = os.path.join(pycachePath,name);
+					if os.path.isfile(fPath):
+						# 重命名文件
+						mtObj = re.match("^(.*)\..*(\..*)$", name);
+						if mtObj:
+							newName = "".join(mtObj.groups());
+							newFPath = os.path.join(pycachePath,newName);
+							os.rename(fPath, newFPath);
+							fPath = newFPath;
+						# 移动文件
+						shutil.move(fPath, os.path.join(pycachePath, ".."));
+				# 删除__pycache__文件夹
+				shutil.rmtree(pycachePath);
 			for name in files:
 				if name.endswith(".py"):
 					os.remove(os.path.join(root,name));
