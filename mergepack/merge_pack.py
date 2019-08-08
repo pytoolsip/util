fd601ddca8a20cbcab0f0e9924b892c0f3e43e8d
JinZhang <2636423765@qq.com>
2019-08-08 19:28:58 +0800

    feat:新增过滤相同引用模块的逻辑

--- mergepack/merge_pack.py
+++ mergepack/merge_pack.py
@@ -2,17 +2,26 @@
 # @Author: JinZhang
 # @Date:   2019-08-08 17:50:27
 # @Last Modified by:   JinZhang
-# @Last Modified time: 2019-08-08 19:14:23
+# @Last Modified time: 2019-08-08 19:28:01
 import os, re;
+
+# 导入过滤
+importMap = [];
 
 # 根据文件夹，获取内容
 def writeContentByFile(path, file):
 	with open(path, "r", encoding = "utf-8") as f:
 		for line in f.readlines():
-			mtObj = re.match("^from (.*) import \*.*#\s*local", line);
-			if mtObj:
-				writeContentByFile(mtObj.group(1).strip()+".py", file);
+			mt = re.match("^from (.*) import \*.*#\s*local", line);
+			if mt:
+				writeContentByFile(mt.group(1).strip()+".py", file);
 			elif not re.match("^#.*", line):
+				mt1 = re.match("^.*(import \w+).*", line);
+				if mt1:
+					ip = mt1.group(1);
+					if ip in importMap:
+						continue;
+					importMap.append(ip);
 				file.write(line);
 
 # 合并文件内容，并输出到指定文件
@@ -30,4 +39,4 @@ if __name__ == '__main__':
 	tgtPath = "installer.py"; # 目标文件路径
 	if os.path.exists("main.py"):
 		merge("main.py", tgtPath);
-		pack(tgtPath);
+		# pack(tgtPath);
